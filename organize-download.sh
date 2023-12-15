1ad0f75f460e7ba7dde590a05f52633429906c7f
radinals <radinalsaragih@gmail.com>
2023-12-15 10:16:27 +0700

    minor tweak

--- organize-download.sh
+++ organize-download.sh
@@ -1,15 +1,13 @@
 #!/usr/bin/bash
-DownloadDir="$HOME/Downloads"
-DownloadDirContent=( "$DownloadDir"/* )
+DownloadDirFiles=( "$HOME/Downloads"/* )
 DownloadStorage="$DownloadDir/.storage"
-for file in "${DownloadDirContent[@]}"
+for file in "${DownloadDirFiles[@]}"
 do
-	if [ ! -d "$file" ]; then
-		tStamp="$(stat -c "%y" "$file" | cut -d " " -f1)"
-		fType="$(file -p -b "$file" | cut -d " " -f1)"
-		mkdir -p "$DownloadStorage/$tStamp/$fType"
-		mv -n "$file" "$DownloadStorage/$tStamp/$fType" && \
-			echo "moved $file to $tStamp/$fType" || \
-			echo "failed to move $file $tStamp"
-	fi
+	[ ! -f "$file" ] && continue
+    tStamp="$(stat -c "%y" "$file" | cut -d " " -f1)"
+    fType="$(file -p -b "$file" | cut -d " " -f1)"
+    mkdir -p "$DownloadStorage/$tStamp/$fType"
+    mv -n "$file" "$DownloadStorage/$tStamp/$fType" && \
+        echo "moved $file to $tStamp/$fType" || \
+        echo "failed to move $file $tStamp"
 done
