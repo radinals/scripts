68711fa3fca375009ae675ab14ba1266aabc2e1c
radinals <radinalsaragih@gmail.com>
2024-05-09 11:51:30 +0700

    simplify logic

--- picomd.sh
+++ picomd.sh
@@ -5,11 +5,14 @@ battery_num=1
 battery_info="/sys/class/power_supply/BAT$battery_num"
 [ ! -d "$battery_info" ] && exit 1
 
-while [[ true ]]; do
+while true ; do
 
     battery_status="$(cat "$battery_info/status")"
-    grep -q -o "Discharging" <<< "$battery_status" && [ "$(pgrep picom)" ] && killall picom >/dev/null
-    grep -q -o "Not charging" <<< "$battery_status" || grep -q -o "Charging" <<< "$battery_status" && [ ! "$(pgrep picom)" ] && exec picom -b >/dev/null &
+    if grep -q -o "Discharging" <<< "$battery_status"; then
+        [ "$(pgrep picom)" ] && killall picom >/dev/null
+    else
+        [ ! "$(pgrep picom)" ] && exec picom -b >/dev/null &
+    fi
 
     sleep 60s
 
